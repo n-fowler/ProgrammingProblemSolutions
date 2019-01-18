7002ba44c84634b829d60785ce5496b2e6e0f2c8
n-fowler <nfowler587@gmail.com>
2019-01-18 13:04:21 -0500

    solved RangeSumOfBstProcessor

---
+++ WindowsFormsApp1/ProgrammingProblemCode/RangeSumOfBstProcessor.cs
@@ -0,0 +1,37 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class RangeSumOfBstProcessor
+    {
+        //val, left, right exist in leetcode already
+        //int sum = 0;
+
+        //public int RangeSumBST(TreeNode root, int L, int R)
+        //{
+        //    Traverse(root, L, R);
+        //    return sum;
+        //}
+
+        //private void Traverse(TreeNode node, int L, int R)
+        //{
+        //    if (node == null)
+        //    {
+        //        return;
+        //    }
+
+        //    if (node.val >= L && node.val <= R)
+        //    {
+        //        sum += node.val;
+        //    }
+
+        //    if (node.val > L)
+        //    {
+        //        Traverse(node.left, L, R);
+        //    }
+
+        //    if (node.val < R)
+        //    {
+        //        Traverse(node.right, L, R);
+        //    }
+        //}
+    }
+}
