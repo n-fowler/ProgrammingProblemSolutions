69a2e1a8706cf705443f3e103fa4a2ada916ac87
n-fowler <nfowler587@gmail.com>
2019-01-27 01:35:27 -0500

    fizzbuzz

---
+++ ProgrammingProblemSolutions/InterviewProblems/FizzBuzzProblem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.InterviewProblems
+{
+    public class FizzBuzzProblem
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> result = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.InterviewProblems;
+using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class FizzBuzz
+    {
+        [TestCase()]
+        public void FizzBuzzTest()
+        {
+            IList<string> expectedResult = new List<string>
+            {
+                "1",
+                "2",
+                "Fizz",
+                "4",
+                "Buzz",
+                "Fizz",
+                "7",
+                "8",
+                "Fizz",
+                "Buzz",
+                "11",
+                "Fizz",
+                "13",
+                "14",
+                "FizzBuzz"
+            };
+
+            IList<string> actualResult = new FizzBuzzProblem().FizzBuzz(15);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
