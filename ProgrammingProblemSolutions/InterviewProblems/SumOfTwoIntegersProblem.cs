feef6b8ab99117134ee6d309df749af8704122c2
n-fowler <nfowler587@gmail.com>
2019-01-28 03:50:03 -0500

    sum of two integers

---
+++ ProgrammingProblemSolutions/InterviewProblems/SumOfTwoIntegersProblem.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammingProblemSolutions.InterviewProblems
+{
+    public class SumOfTwoIntegersProblem
+    {
+        public int GetSum(int a, int b)
+        {
+            return b == 0 ? a : GetSum(a ^ b, (a & b) << 1);
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/SumOfTwoIntegers.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.InterviewProblems;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class SumOfTwoIntegers
+    {
+        [TestCase(1, 2, ExpectedResult = 3)]
+        [TestCase(-2, 3, ExpectedResult = 1)]
+        public int SumOfTwoIntegersTest(int a, int b)
+        {
+            return new SumOfTwoIntegersProblem().GetSum(a, b);
+        }
+    }
+}
