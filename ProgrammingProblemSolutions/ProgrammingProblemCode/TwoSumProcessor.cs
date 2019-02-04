9ed1df71ccbc362083b9ef3d8dce8e8f6746b591
n-fowler <nfowler587@gmail.com>
2019-02-04 09:08:53 -0500

    two sum problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/TwoSumProcessor.cs
@@ -0,0 +1,26 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class TwoSumProcessor
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/TwoSum.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class TwoSum
+    {
+        [TestCase()]
+        public void TwoSumTest()
+        {
+            int[] inputArray = { 2, 7, 11, 15 };
+            int target = 9;
+
+            int[] expectedOutput = { 0, 1 };
+
+            Assert.AreEqual(expectedOutput, new TwoSumProcessor().TwoSum(inputArray, target));
+        }
+    }
+}
