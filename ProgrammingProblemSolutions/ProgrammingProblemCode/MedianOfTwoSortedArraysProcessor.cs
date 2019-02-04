365aafb01a88a61381eedaad4e00b5c0bffcea90
n-fowler <nfowler587@gmail.com>
2019-02-04 10:27:46 -0500

    Median of 2 sorted arrays

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/MedianOfTwoSortedArraysProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class MedianOfTwoSortedArraysProcessor
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            double answer;
+
+            int[] nums = new int[nums1.Length + nums2.Length];
+            Array.Copy(nums1, 0, nums, 0, nums1.Length);
+            Array.Copy(nums2, 0, nums, nums1.Length, nums2.Length);
+            Array.Sort(nums, Comparer<int>.Default);
+
+            if (nums.Length % 2 == 0) //even
+            {
+                answer = (nums[(nums.Length / 2) - 1] + nums[(nums.Length / 2)]) / 2.0;
+            }
+            else //odd
+            {
+                answer = nums[Convert.ToInt32(Math.Round((nums.Length - 1) / 2.0))];
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class MedianOfTwoSortedArrays
+    {
+        [TestCase()]
+        public void MedianOfTwoSortedArraysTest1()
+        {
+            int[] inputArrayOne = { 1, 3 };
+            int[] inputArrayTwo = { 2 };
+
+            Assert.AreEqual(2.0, new MedianOfTwoSortedArraysProcessor().FindMedianSortedArrays(inputArrayOne, inputArrayTwo));
+        }
+
+        [TestCase()]
+        public void MedianOfTwoSortedArraysTest2()
+        {
+            int[] inputArrayOne = { 1, 2 };
+            int[] inputArrayTwo = { 3, 4 };
+
+            Assert.AreEqual(2.5, new MedianOfTwoSortedArraysProcessor().FindMedianSortedArrays(inputArrayOne, inputArrayTwo));
+        }
+
+        [TestCase()]
+        public void MedianOfTwoSortedArraysTest3()
+        {
+            int[] inputArrayOne = { };
+            int[] inputArrayTwo = { 1 };
+
+            Assert.AreEqual(1.0, new MedianOfTwoSortedArraysProcessor().FindMedianSortedArrays(inputArrayOne, inputArrayTwo));
+        }
+
+        [TestCase()]
+        public void MedianOfTwoSortedArraysTest4()
+        {
+            int[] inputArrayOne = { 3 };
+            int[] inputArrayTwo = { -2, -1 };
+
+            Assert.AreEqual(-1.0, new MedianOfTwoSortedArraysProcessor().FindMedianSortedArrays(inputArrayOne, inputArrayTwo));
+        }
+    }
+}
