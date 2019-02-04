6137e4d1cf6487324ae21c9475b6129353e3448c
n-fowler <nfowler587@gmail.com>
2019-02-04 09:09:32 -0500

    shortest distance problem (terrible, faster than 68% but the worst memory usage possible)

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ShortestDistanceToCharacterProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ShortestDistanceToCharacterProcessor
+    {
+        public int[] ShortestToChar(string S, char C)
+        {
+            char[] schar = S.ToCharArray();
+            int[] answer = new int[schar.Length];
+            List<int> charIndexes = GetCharIndexes(S, C, schar);
+
+            for (int i = 0; i < schar.Length; i++)
+            {
+                int minDistance = schar.Length;
+                foreach (int charIndex in charIndexes)
+                {
+                    if (charIndex - i < minDistance && charIndex - i >= 0)
+                    {
+                        minDistance = charIndex - i;
+                    }
+
+                    if (-1 * (charIndex - i) < minDistance && -1 * (charIndex - i) >= 0)
+                    {
+                        minDistance = -1 * (charIndex - i);
+                    }
+                }
+
+                answer[i] = minDistance;
+            }
+
+            return answer;
+        }
+
+        private List<int> GetCharIndexes(string S, char C, char[] schar)
+        {
+            int startIndex = 0;
+            List<int> charIndexes = new List<int>();
+
+            while (S.IndexOf(C, startIndex, schar.Length - startIndex) != -1)
+            {
+                startIndex = S.IndexOf(C, startIndex, schar.Length - startIndex);
+                charIndexes.Add(startIndex);
+                startIndex++;
+            }
+
+            return charIndexes;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ShortestDistanceToCharacter.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ShortestDistanceToCharacter
+    {
+        [TestCase()]
+        public void ShortestDistanceToCharacterTest1()
+        {
+            string inputString = "loveleetcode";
+            char inputChar = 'e';
+
+            int[] expectedOutput = { 3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0 };
+
+            Assert.AreEqual(expectedOutput, new ShortestDistanceToCharacterProcessor().ShortestToChar(inputString, inputChar));
+        }
+
+        [TestCase()]
+        public void ShortestDistanceToCharacterTest2()
+        {
+            string inputString = "aaba";
+            char inputChar = 'b';
+
+            int[] expectedOutput = { 2, 1, 0, 1 };
+
+            Assert.AreEqual(expectedOutput, new ShortestDistanceToCharacterProcessor().ShortestToChar(inputString, inputChar));
+        }
+
+        [TestCase()]
+        public void ShortestDistanceToCharacterTest3()
+        {
+            string inputString = "abaa";
+            char inputChar = 'b';
+
+            int[] expectedOutput = { 1, 0, 1, 2 };
+
+            Assert.AreEqual(expectedOutput, new ShortestDistanceToCharacterProcessor().ShortestToChar(inputString, inputChar));
+        }
+    }
+}
