353a59934e9561a87df783ff1030f0daa69e888b
n-fowler <nfowler587@gmail.com>
2019-02-04 07:41:50 -0500

    subdomain visit count

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/SubdomainVisitCountProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class SubdomainVisitCountProcessor
+    {
+        public IList<string> SubdomainVisits(string[] cpdomains)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (string cpdomain in cpdomains)
+            {
+                int count = Convert.ToInt32(cpdomain.Split(' ')[0]);
+                string[] subdomains = cpdomain.Split(' ')[1].Split('.');
+
+                for (int i = 0; i < subdomains.Length; i++)
+                {
+                    string subdomain = string.Join('.', subdomains, i, subdomains.Length - i);
+                    if (dict.ContainsKey(subdomain))
+                    {
+                        dict[subdomain] += count;
+                    }
+                    else
+                    {
+                        dict.Add(subdomain, count);
+                    }
+                }
+            }
+
+            return dict.Select(keyValuePair => $"{keyValuePair.Value} {keyValuePair.Key}").ToList();
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/SubdomainVisitCount.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class SubdomainVisitCount
+    {
+        [TestCase()]
+        public void SubDomainVisitCountTest1()
+        {
+            string[] inputArray = { "9001 discuss.leetcode.com" };
+            string[] expectedOutput = { "9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com" };
+
+            Assert.AreEqual(expectedOutput, new SubdomainVisitCountProcessor().SubdomainVisits(inputArray));
+        }
+
+        [TestCase()]
+        public void SubDomainVisitCountTest2()
+        {
+            string[] inputArray = { "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org" };
+            string[] expectedOutput = { "900 google.mail.com", "901 mail.com", "951 com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org", "5 org" };
+
+            Assert.AreEqual(expectedOutput, new SubdomainVisitCountProcessor().SubdomainVisits(inputArray));
+        }
+    }
+}
