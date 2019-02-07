36343fc2aa939046c3180fa9d75f8930f148cbf5
n-fowler <nfowler587@gmail.com>
2019-02-07 11:59:26 -0500

    perfect number (slow, will be improved)

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/PerfectNumberProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class PerfectNumberProcessor
+    {
+        public bool CheckPerfectNumber(int num)
+        {
+            List<int> divisors = new List<int>();
+
+            if (num <= 0)
+            {
+                return false;
+            }
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+
+            int sum = divisors.Sum();
+
+            return sum == num;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/PerfectNumber.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class PerfectNumber
+    {
+        [TestCase(28, ExpectedResult = true)]
+        public bool PerfectNumberTest1(int number)
+        {
+            return new PerfectNumberProcessor().CheckPerfectNumber(number);
+        }
+    }
+}
