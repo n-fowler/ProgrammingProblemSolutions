7d865143f9ea771034830eac67871864acf23b06
n-fowler <nfowler587@gmail.com>
2019-02-07 11:49:02 -0500

    self dividing numbers

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/SelfDividingNumbersProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class SelfDividingNumbersProcessor
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            List<int> results = new List<int>();
+
+            for (int i = left; i <= right; i++)
+            {
+                if (IsSelfDividing(i))
+                {
+                    results.Add(i);
+                }
+            }
+
+            return results;
+        }
+
+        private bool IsSelfDividing(int number)
+        {
+            foreach (char c in number.ToString())
+            {
+                if (c == '0')
+                {
+                    return false;
+                }
+
+                if (number % (Convert.ToInt32(c) - 48) != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/SelfDividingNumbers.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class SelfDividingNumbers
+    {
+        [TestCase()]
+        public void SelfDividingNumbersTest()
+        {
+            int left = 1;
+            int right = 22;
+            int[] expectedOutput = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22 };
+
+            Assert.AreEqual(expectedOutput, new SelfDividingNumbersProcessor().SelfDividingNumbers(left, right));
+        }
+    }
+}
