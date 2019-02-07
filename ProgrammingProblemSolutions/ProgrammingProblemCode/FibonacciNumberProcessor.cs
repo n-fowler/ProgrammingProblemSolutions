3525e221d7c6090be1441d0f6d91d20bbca939bc
n-fowler <nfowler587@gmail.com>
2019-02-07 06:41:59 -0500

    fib number

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/FibonacciNumberProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class FibonacciNumberProcessor
+    {
+        //The problem states N is less than or equal to 30
+        public int Fib(int N)
+        {
+            int[] fibonacciNumbers = new int[31];
+            fibonacciNumbers[0] = 0;
+            fibonacciNumbers[1] = 1;
+
+            for (int n = 2; n < fibonacciNumbers.Length; n++)
+            {
+                fibonacciNumbers[n] = fibonacciNumbers[n - 1] + fibonacciNumbers[n - 2];
+            }
+
+            return fibonacciNumbers[N];
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/FibonacciNumber.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class FibonacciNumber
+    {
+        [TestCase(2, ExpectedResult = 1)]
+        [TestCase(3, ExpectedResult = 2)]
+        [TestCase(4, ExpectedResult = 3)]
+        [TestCase(5, ExpectedResult = 5)]
+        public int FibonacciNumberTest(int number)
+        {
+            return new FibonacciNumberProcessor().Fib(number);
+        }
+    }
+}
