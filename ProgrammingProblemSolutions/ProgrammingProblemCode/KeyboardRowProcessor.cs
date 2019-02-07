e7931d7fb239aa89c035a9c83e9fe12d1cc58755
n-fowler <nfowler587@gmail.com>
2019-02-07 07:00:43 -0500

    keyboard row problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/KeyboardRowProcessor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class KeyboardRowProcessor
+    {
+        public string[] FindWords(string[] words)
+        {
+            List<string> resultWords = new List<string>();
+
+            foreach (string word in words)
+            {
+                string lowerCaseWord = word.ToLower();
+
+                if (IsTopRowWord(lowerCaseWord) || isMiddleRowWord(lowerCaseWord) || isBottomRowWord(lowerCaseWord))
+                {
+                    resultWords.Add(word);
+                }
+            }
+
+            return resultWords.ToArray();
+        }
+
+        private bool IsTopRowWord(string word)
+        {
+            char[] topRowCharacters = "qwertyuiop".ToCharArray();
+            char[] chars = word.ToCharArray();
+            foreach (char c in chars)
+            {
+                if (!topRowCharacters.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool isMiddleRowWord(string word)
+        {
+            char[] middleRowCharacters = "asdfghjkl".ToCharArray();
+            char[] chars = word.ToCharArray();
+            foreach (char c in chars)
+            {
+                if (!middleRowCharacters.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool isBottomRowWord(string word)
+        {
+            char[] bottomRowCharacters = "zxcvbnm".ToCharArray();
+            char[] chars = word.ToCharArray();
+            foreach (char c in chars)
+            {
+                if (!bottomRowCharacters.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/KeyboardRow.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture]
+    public class KeyboardRow
+    {
+        [TestCase()]
+        public void KeyboardRowTest()
+        {
+            string[] inputArray = { "Hello", "Alaska", "Dad", "Peace" };
+            string[] expectedOutput = { "Alaska", "Dad" };
+
+            Assert.AreEqual(expectedOutput, new KeyboardRowProcessor().FindWords(inputArray));
+        }
+    }
+}
