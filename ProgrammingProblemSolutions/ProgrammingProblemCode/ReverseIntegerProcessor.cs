3da3f9911caafc9b4c1d8dede5c68c470de00133
n-fowler <nfowler587@gmail.com>
2019-02-08 12:07:16 -0500

    Reverse Integer problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ReverseIntegerProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ReverseIntegerProcessor
+    {
+        public int Reverse(int x)
+        {
+            bool isNegative = false;
+            if (x < 0)
+            {
+                x *= -1;
+                isNegative = true;
+            }
+
+            IEnumerable<char> chars = x.ToString().ToCharArray().Reverse();
+            char[] resultChars = new char[chars.Count()];
+
+            for (int i = 0; i < chars.Count(); i++)
+            {
+                resultChars[i] = chars.ToArray()[i];
+            }
+
+            string resultString = new string(resultChars);
+
+            try
+            {
+                if (isNegative)
+                {
+                    return -1 * Convert.ToInt32(resultString);
+                }
+                return Convert.ToInt32(resultString);
+            }
+            catch (Exception)
+            {
+                //Eat the exception
+                return 0;
+            }
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ReverseInteger.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ReverseInteger
+    {
+        [TestCase(123, ExpectedResult = 321)]
+        [TestCase(-123, ExpectedResult = -321)]
+        [TestCase(120, ExpectedResult = 21)]
+        [TestCase(1534236469, ExpectedResult = 0)]
+        public int ReverseIntegerTest(int number)
+        {
+            return new ReverseIntegerProcessor().Reverse(number);
+        }
+    }
+}
