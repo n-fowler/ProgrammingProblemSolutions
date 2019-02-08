f9b74b7ff91f173d6ecf06dc2807fa859f5c8ef2
n-fowler <nfowler587@gmail.com>
2019-02-08 11:36:11 -0500

    longest substring without repeating characters problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/LongestSubstringWithoutRepeatingCharactersProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class LongestSubstringWithoutRepeatingCharactersProcessor
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            List<char> chars = new List<char>();
+            foreach (char c in s)
+            {
+                chars.Add(c);
+                if (chars.Distinct().Count() < chars.Count)
+                {
+                    chars.RemoveAt(0);
+                }
+            }
+
+            return chars.Count;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        [TestCase("abcabcbb", ExpectedResult = 3)]
+        [TestCase("bbbbb", ExpectedResult = 1)]
+        [TestCase("pwwkew", ExpectedResult = 3)]
+        [TestCase(" ", ExpectedResult = 1)]
+        [TestCase("aab", ExpectedResult = 2)]
+        [TestCase("dvdf", ExpectedResult = 3)]
+        public int LongestSubstringWithoutRepeatingCharactersTest(string s)
+        {
+            return new LongestSubstringWithoutRepeatingCharactersProcessor().LengthOfLongestSubstring(s);
+        }
+    }
+}
