ba48206bd6af865d85e4760b0aeba57b45f70370
n-fowler <nfowler587@gmail.com>
2019-02-09 11:56:55 -0500

    zig zag problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ZigZagConversionProcessor.cs
@@ -0,0 +1,42 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ZigZagConversionProcessor
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows <= 1)
+            {
+                return s;
+            }
+
+            string[] resultStrings = new string[numRows];
+
+            int index = 0;
+
+            while (index < s.Length)
+            {
+                for (int i = 0; i < numRows; i++)
+                {
+                    if (index < s.Length)
+                    {
+                        resultStrings[i] += s.Substring(index, 1);
+                    }
+
+                    index++;
+                }
+
+                for (int i = numRows - 2; i > 0; i--)
+                {
+                    if (index < s.Length)
+                    {
+                        resultStrings[i] += s.Substring(index, 1);
+                    }
+
+                    index++;
+                }
+            }
+
+            return string.Join("", resultStrings);
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ZigZagConversion.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ZigZagConversion
+    {
+        [TestCase("PAYPALISHIRING", 3, ExpectedResult = "PAHNAPLSIIGYIR")]
+        [TestCase("PAYPALISHIRING", 4, ExpectedResult = "PINALSIGYAHRPI")]
+        public string ZigZagConversionTest(string s, int numRows)
+        {
+            return new ZigZagConversionProcessor().Convert(s, numRows);
+        }
+    }
+}
