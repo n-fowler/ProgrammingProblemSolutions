58ad55bb363ff85e9ed01b00dee87a64b843daee
n-fowler <nfowler587@gmail.com>
2019-02-10 10:32:12 -0500

    validate parenthesis problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ValidateParenthesisProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ValidateParenthesisProcessor
+    {
+        public int MinAddToMakeValid(string S)
+        {
+            while (S.IndexOf("()", StringComparison.Ordinal) > -1)
+            {
+                S = S.Remove(S.IndexOf("()", StringComparison.Ordinal), 2);
+            }
+
+            return S.Length;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ValidateParenthesis.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    class ValidateParenthesis
+    {
+        [TestCase("())", ExpectedResult = 1)]
+        [TestCase("(((", ExpectedResult = 3)]
+        [TestCase("()", ExpectedResult = 0)]
+        [TestCase("()))((", ExpectedResult = 4)]
+        public int ValidateParenthesisTest(string s)
+        {
+            return new ValidateParenthesisProcessor().MinAddToMakeValid(s);
+        }
+    }
+}
