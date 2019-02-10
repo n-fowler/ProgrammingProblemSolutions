e844cf0c2ef9f51eccb2c2f1017a2d72c04cf607
n-fowler <nfowler587@gmail.com>
2019-02-10 10:02:45 -0500

    c# to lower case processor changes

--- ProgrammingProblemSolutions/ProgrammingProblemCode/ToLowerCaseProcessor.cs
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ToLowerCaseProcessor.cs
@@ -2,9 +2,23 @@
 {
     public class ToLowerCaseProcessor
     {
+        //Faster than 95.89%
         public string ToLowerCase(string str)
         {
-            return str.ToLower();
+            string result = string.Empty;
+            foreach (char c in str)
+            {
+                if (c >= 65 && c <= 90)
+                {
+                    result += (char)(c + 32);
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            return result;
         }
     }
 }
