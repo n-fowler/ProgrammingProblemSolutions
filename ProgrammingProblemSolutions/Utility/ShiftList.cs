646c3ee4f1327dbf222b5b74cb3749668761d6b9
n-fowler <nfowler587@gmail.com>
2019-02-11 15:48:37 -0500

    add shiftlist

---
+++ ProgrammingProblemSolutions/Utility/ShiftList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.Utility
+{
+    public static class ShiftList
+    {
+        public static List<T> ShiftLeft<T>(this List<T> list, int shiftBy)
+        {
+            if (list.Count <= shiftBy)
+            {
+                return list;
+            }
+
+            List<T> result = list.GetRange(shiftBy, list.Count - shiftBy);
+            result.AddRange(list.GetRange(0, shiftBy));
+            return result;
+        }
+
+        public static List<T> ShiftRight<T>(this List<T> list, int shiftBy)
+        {
+            if (list.Count <= shiftBy)
+            {
+                return list;
+            }
+
+            List<T> result = list.GetRange(list.Count - shiftBy, shiftBy);
+            result.AddRange(list.GetRange(0, list.Count - shiftBy));
+            return result;
+        }
+    }
+}
