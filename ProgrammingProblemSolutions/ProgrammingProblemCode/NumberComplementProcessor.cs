e3b1a5623ebe377db91a2a67233f1c7e046afee0
n-fowler <nfowler587@gmail.com>
2019-02-12 17:55:42 -0500

    number complement

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/NumberComplementProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class NumberComplementProcessor
+    {
+        //Faster than 99.07%
+        public int FindComplement(int num)
+        {
+            int mask = (1 << (int)Math.Ceiling((Math.Log(num + 1) / Math.Log(2)))) - 1;
+            return ~num & mask;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/NumberComplement.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class NumberComplement
+    {
+        [TestCase(5, ExpectedResult = 2)]
+        [TestCase(1, ExpectedResult = 0)]
+        public int NumberComplementTest(int num)
+        {
+            return new NumberComplementProcessor().FindComplement(num);
+        }
+    }
+}
