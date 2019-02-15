d897bddd3901fe18e316d56f76156ddd167c96c5
n-fowler <nfowler587@gmail.com>
2019-02-15 15:53:35 -0500

    Reverse linked list

---
+++ ProgrammingProblemSolutions/InterviewProblems/ReverseLinkedListProblem.cs
@@ -0,0 +1,20 @@
+﻿using ProgrammingProblemSolutions.Utility;
+
+namespace ProgrammingProblemSolutions.InterviewProblems
+{
+    public class ReverseLinkedListProblem
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head?.next == null)
+            {
+                return head;
+            }
+
+            ListNode node = ReverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return node;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ReverseLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.InterviewProblems;
+using ProgrammingProblemSolutions.Utility;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ReverseLinkedList
+    {
+        [TestCase()]
+        public void ReverseLinkedListTest()
+        {
+            ListNode nodeOne = new ListNode(1);
+            ListNode nodeTwo = new ListNode(2);
+            ListNode nodeThree = new ListNode(3);
+            ListNode nodeFour = new ListNode(4);
+            ListNode nodeFive = new ListNode(5);
+            nodeOne.next = nodeTwo;
+            nodeTwo.next = nodeThree;
+            nodeThree.next = nodeFour;
+            nodeFour.next = nodeFive;
+
+            ListNode actualHead = new ReverseLinkedListProblem().ReverseList(nodeOne);
+
+            Assert.AreEqual(nodeFive, actualHead);
+            Assert.AreEqual(nodeFour, nodeFive.next);
+            Assert.AreEqual(nodeThree, nodeFour.next);
+            Assert.AreEqual(nodeTwo, nodeThree.next);
+            Assert.AreEqual(nodeOne, nodeTwo.next);
+            Assert.IsNull(nodeOne.next);
+        }
+    }
+}
