eb0afbb7209affecd5ca65514c61a68475132b6b
n-fowler <nfowler587@gmail.com>
2019-02-15 14:41:16 -0500

    Missing number problem

---
+++ ProgrammingProblemSolutions/InterviewProblems/MissingNumberProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.InterviewProblems
+{
+    public class MissingNumberProblem
+    {
+        public int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length + 1; i++)
+            {
+                if (i == nums.Length)
+                {
+                    return nums.Length;
+                }
+
+                if (nums[i] != i)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/MissingNumber.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.InterviewProblems;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class MissingNumber
+    {
+        [TestCase()]
+        public void MissingNumberTest1()
+        {
+            int[] inputArray = { 3, 0, 1 };
+            int expectedResult = 2;
+
+            Assert.AreEqual(expectedResult, new MissingNumberProblem().MissingNumber(inputArray));
+        }
+
+        [TestCase()]
+        public void MissingNumberTest2()
+        {
+            int[] inputArray = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            int expectedResult = 8;
+
+            Assert.AreEqual(expectedResult, new MissingNumberProblem().MissingNumber(inputArray));
+        }
+
+        [TestCase()]
+        public void MissingNumberTest3()
+        {
+            int[] inputArray = { 0 };
+            int expectedResult = 1;
+
+            Assert.AreEqual(expectedResult, new MissingNumberProblem().MissingNumber(inputArray));
+        }
+
+        [TestCase()]
+        public void MissingNumberTest4()
+        {
+            int[] inputArray = { 0, 1 };
+            int expectedResult = 2;
+
+            Assert.AreEqual(expectedResult, new MissingNumberProblem().MissingNumber(inputArray));
+        }
+    }
+}
