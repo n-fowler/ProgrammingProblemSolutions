9fd868dbc18bb38910f769f6d0d18cdea025758d
n-fowler <nfowler587@gmail.com>
2019-03-01 09:36:42 -0500

    available captures for rook test

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/AvailableCapturesForRookProcessor.cs
@@ -0,0 +1,138 @@
+﻿using System.Drawing;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class AvailableCapturesForRookProcessor
+    {
+        private char[][] ChessBoard { get; set; }
+        private int PossibleCaptures { get; set; }
+
+        public int NumRookCaptures(char[][] board)
+        {
+            ChessBoard = board;
+
+            var rookPosition = GetRookPosition();
+            ScanLeft(rookPosition.X, rookPosition.Y);
+            ScanRight(rookPosition.X, rookPosition.Y);
+            ScanDown(rookPosition.X, rookPosition.Y);
+            ScanUp(rookPosition.X, rookPosition.Y);
+
+            return PossibleCaptures;
+        }
+
+        private enum Piece
+        {
+            Rook = 'R',
+            Bishop = 'B',
+            Pawn = 'p',
+            EmptySpace = '.'
+        }
+
+        private Piece GetPiece(char c)
+        {
+            switch (c)
+            {
+                case 'R':
+                    return Piece.Rook;
+                case 'B':
+                    return Piece.Bishop;
+                case 'p':
+                    return Piece.Pawn;
+                default:
+                    return Piece.EmptySpace;
+            }
+        }
+
+        private Point GetRookPosition()
+        {
+            for (int i = 0; i < ChessBoard.Length; i++)
+            {
+                for (int j = 0; j < ChessBoard[i].Length; j++)
+                {
+                    if (GetPiece(ChessBoard[i][j]) == Piece.Rook)
+                    {
+                        return new Point(j, i);
+                    }
+                }
+            }
+
+            return new Point(-1, -1);
+        }
+
+        private void ScanLeft(int x, int y)
+        {
+            for (int i = x; i >= 0; i--)
+            {
+                var currentPiece = GetPiece(ChessBoard[y][i]);
+
+                if (currentPiece == Piece.Bishop)
+                {
+                    return;
+                }
+
+                if (currentPiece == Piece.Pawn)
+                {
+                    PossibleCaptures++;
+                    return;
+                }
+            }
+        }
+
+        private void ScanRight(int x, int y)
+        {
+            for (int i = x; i < ChessBoard.Length; i++)
+            {
+                var currentPiece = GetPiece(ChessBoard[y][i]);
+
+                if (currentPiece == Piece.Bishop)
+                {
+                    return;
+                }
+
+                if (currentPiece == Piece.Pawn)
+                {
+                    PossibleCaptures++;
+                    return;
+                }
+            }
+        }
+
+        private void ScanDown(int x, int y)
+        {
+            for (int i = y; i >= 0; i--)
+            {
+                var currentPiece = GetPiece(ChessBoard[i][x]);
+
+                if (currentPiece == Piece.Bishop)
+                {
+                    return;
+                }
+
+                if (currentPiece == Piece.Pawn)
+                {
+                    PossibleCaptures++;
+                    return;
+                }
+            }
+        }
+
+        private void ScanUp(int x, int y)
+        {
+            for (int i = y; i < ChessBoard.Length; i++)
+            {
+                var currentPiece = GetPiece(ChessBoard[i][x]);
+
+                if (currentPiece == Piece.Bishop)
+                {
+                    return;
+                }
+
+                if (currentPiece == Piece.Pawn)
+                {
+                    PossibleCaptures++;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/AvailableCapturesForRook.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class AvailableCapturesForRook
+    {
+        [TestCase()]
+        public void AvailableCapturesForRookTestOne()
+        {
+            char[][] testBoard = new[]
+            {
+                new[] {'.', '.', '.', '.', '.', '.', '.', '.'},
+                new[] {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                new[] {'.', '.', '.', 'R', '.', '.', '.', 'p'},
+                new[] {'.', '.', '.', '.', '.', '.', '.', '.'},
+                new[] {'.', '.', '.', '.', '.', '.', '.', '.'},
+                new[] {'.', '.', '.', 'p', '.', '.', '.', '.'},
+                new[] {'.', '.', '.', '.', '.', '.', '.', '.'},
+                new[] {'.', '.', '.', '.', '.', '.', '.', '.'}
+            };
+
+            Assert.AreEqual(3, new AvailableCapturesForRookProcessor().NumRookCaptures(testBoard));
+        }
+
+        [TestCase()]
+        public void AvailableCapturesForRookTestTwo()
+        {
+            char[][] testBoard = new[]
+            {
+                new[] {'.','.','.','.','.','.','.','.'},
+                new[] {'.','p','p','p','p','p','.','.'},
+                new[] {'.','p','p','B','p','p','.','.'},
+                new[] {'.','p','B','R','B','p','.','.'},
+                new[] {'.','p','p','B','p','p','.','.'},
+                new[] {'.','p','p','p','p','p','.','.'},
+                new[] {'.','.','.','.','.','.','.','.'},
+                new[] {'.','.','.','.','.','.','.','.'}
+            };
+
+            Assert.AreEqual(0, new AvailableCapturesForRookProcessor().NumRookCaptures(testBoard));
+        }
+
+        [TestCase()]
+        public void AvailableCapturesForRookTestThree()
+        {
+            char[][] testBoard = new[]
+            {
+                new[] {'.','.','.','.','.','.','.','.'},
+                new[] {'.','.','.','p','.','.','.','.'},
+                new[] {'.','.','.','p','.','.','.','.'},
+                new[] {'p','p','.','R','.','p','B','.'},
+                new[] {'.','.','.','.','.','.','.','.'},
+                new[] {'.','.','.','B','.','.','.','.'},
+                new[] {'.','.','.','p','.','.','.','.'},
+                new[] {'.','.','.','.','.','.','.','.'}
+            };
+
+            Assert.AreEqual(3, new AvailableCapturesForRookProcessor().NumRookCaptures(testBoard));
+        }
+    }
+}
