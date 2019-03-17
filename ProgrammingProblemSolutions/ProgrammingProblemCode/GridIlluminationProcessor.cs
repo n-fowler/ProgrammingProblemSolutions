3807d69ae025621ab6e344e103d953dae0c76a8a
n-fowler <nfowler587@gmail.com>
2019-03-16 21:27:14 -0400

    non optimized grid illumination

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/GridIlluminationProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class GridIlluminationProcessor
+    {
+        public int[] GridIllumination(int N, int[][] lamps, int[][] queries)
+        {
+            int[] result = new int[queries.Length];
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                result[i] = IsIlluminated(lamps, queries[i]) ? 1 : 0;
+            }
+
+            return result;
+        }
+
+        private bool IsIlluminated(int[][] lightCoordinates, int[] queryCoordinates)
+        {
+            bool result = false;
+
+            for (int index = 0; index < lightCoordinates.Length; index++)
+            {
+                int[] light = lightCoordinates[index];
+
+                //Is the light turned on?
+                if (light[0] == -1 || light[1] == -1)
+                {
+                    //It's turned off...
+                    continue;
+                }
+
+                int dy = queryCoordinates[1] - light[1];
+                int dx = queryCoordinates[0] - light[0];
+                if (dx == 0 || dy == 0 || Math.Abs(dx) == Math.Abs(dy))
+                {
+                    result = true;
+
+                    //Turn the light off if it's adjacent 8-directionally
+                    if ((dx == 0 && Math.Abs(dy) <= 1) || (dy == 0 && Math.Abs(dx) <= 1) || (Math.Abs(dx) == Math.Abs(dy) && Math.Abs(dx) <= 1 && Math.Abs(dy) <= 1))
+                    {
+                        lightCoordinates[index] = new[] { -1, -1 };
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/GridIllumination.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class GridIllumination
+    {
+        [TestCase]
+        public void GridIlluminationTestOne()
+        {
+            int gridSize = 5;
+            int[][] lamps = { new[] { 0, 0 }, new[] { 4, 4 } };
+            int[][] queries = { new[] { 1, 1 }, new[] { 1, 0 } };
+
+            int[] expectedResult = { 1, 0 };
+
+            Assert.AreEqual(expectedResult, new GridIlluminationProcessor().GridIllumination(gridSize, lamps, queries));
+        }
+
+        [TestCase]
+        public void GridIlluminationTestTwo()
+        {
+            int gridSize = 5;
+            int[][] lamps = { new[] { 0, 0 }, new[] { 1, 0 } };
+            int[][] queries = { new[] { 1, 1 }, new[] { 1, 1 } };
+
+            int[] expectedResult = { 1, 0 };
+
+            Assert.AreEqual(expectedResult, new GridIlluminationProcessor().GridIllumination(gridSize, lamps, queries));
+        }
+
+        [TestCase]
+        public void GridIlluminationTestThree()
+        {
+            int gridSize = 5;
+            int[][] lamps = { new[] { 0, 0 }, new[] { 4, 4 } };
+            int[][] queries = { new[] { 1, 1 }, new[] { 1, 1 } };
+
+            int[] expectedResult = { 1, 1 };
+
+            Assert.AreEqual(expectedResult, new GridIlluminationProcessor().GridIllumination(gridSize, lamps, queries));
+        }
+
+        [TestCase]
+        public void GridIlluminationTestFour()
+        {
+            int gridSize = 5;
+            int[][] lamps = { new[] { 0, 0 }, new[] { 0, 4 } };
+            int[][] queries = { new[] { 0, 4 }, new[] { 0, 1 }, new[] { 1, 4 } };
+
+            int[] expectedResult = { 1, 1, 0 };
+
+            Assert.AreEqual(expectedResult, new GridIlluminationProcessor().GridIllumination(gridSize, lamps, queries));
+        }
+    }
+}
