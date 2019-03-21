33f8aabc6109b60fe46f281ac8b368d8272f25b8
n-fowler <nfowler587@gmail.com>
2019-03-21 18:16:21 -0400

    compliment base ten

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ComplimentBaseTenProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ComplimentBaseTenProcessor
+    {
+        public int BitwiseComplement(int N)
+        {
+            if (N == 0)
+            {
+                return 1;
+            }
+
+            int mask = (1 << (int)Math.Ceiling((Math.Log(N + 1) / Math.Log(2)))) - 1;
+            return ~N & mask;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ComplimentBaseTen.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ComplimentBaseTen
+    {
+        [TestCase(5, ExpectedResult = 2)]
+        [TestCase(7, ExpectedResult = 0)]
+        [TestCase(10, ExpectedResult = 5)]
+        public int ComplimentBaseTenTest(int num)
+        {
+            return new ComplimentBaseTenProcessor().BitwiseComplement(num);
+        }
+    }
+}
