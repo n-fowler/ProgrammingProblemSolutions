b3a58c80a2b3e08c73f5c3d32f1d534b0ff4174f
n-fowler <nfowler587@gmail.com>
2019-03-30 14:52:13 -0400

    goat latin problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/GoatLatinProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class GoatLatinProcessor
+    {
+        public string ToGoatLatin(string S)
+        {
+            string[] words = S.Split(' ');
+            for (int index = 0; index < words.Length; index++)
+            {
+                if (!StartsWithVowel(words[index]))
+                {
+                    string firstLetter = words[index].Substring(0, 1);
+                    words[index] = words[index].Remove(0, 1);
+                    words[index] = words[index] + firstLetter;
+                }
+
+                words[index] = words[index] + "ma" + new string('a', index + 1);
+            }
+
+            return string.Join(' ', words);
+        }
+
+        private bool StartsWithVowel(string word)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            return vowels.Contains(word.ToCharArray()[0]);
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/GoatLatin.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class GoatLatin
+    {
+        [TestCase("I speak Goat Latin", ExpectedResult = "Imaa peaksmaaa oatGmaaaa atinLmaaaaa")]
+        [TestCase("The quick brown fox jumped over the lazy dog", ExpectedResult = "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa")]
+        public string GoatLatinTest(string input)
+        {
+            return new GoatLatinProcessor().ToGoatLatin(input);
+        }
+    }
+}
