08145430ce8da5d38fbda7f23cba13592b9d29c4
n-fowler <nfowler587@gmail.com>
2019-04-02 17:12:42 -0400

    binary prefix div by five

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/BinaryPrefixDivisibleByFiveProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class BinaryPrefixDivisibleByFiveProcessor
+    {
+        public IList<bool> PrefixesDivBy5(int[] A)
+        {
+            List<bool> results = new List<bool>();
+            int remainder = 0;
+            foreach (int a in A)
+            {
+                remainder = ((remainder << 1) + a) % 5;
+                results.Add(remainder == 0);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/BinaryPrefixDivisibleByFive.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+using System.Collections.Generic;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class BinaryPrefixDivisibleByFive
+    {
+        [TestCase()]
+        public void BinaryPrefixDivisibleByFiveTestOne()
+        {
+            int[] inputArray = { 0, 1, 1 };
+            List<bool> expectedResult = new List<bool> { true, false, false };
+
+            Assert.AreEqual(expectedResult, new BinaryPrefixDivisibleByFiveProcessor().PrefixesDivBy5(inputArray));
+        }
+
+        [TestCase()]
+        public void BinaryPrefixDivisibleByFiveTestTwo()
+        {
+            int[] inputArray = { 1, 1, 1 };
+            List<bool> expectedResult = new List<bool> { false, false, false };
+
+            Assert.AreEqual(expectedResult, new BinaryPrefixDivisibleByFiveProcessor().PrefixesDivBy5(inputArray));
+        }
+
+        [TestCase()]
+        public void BinaryPrefixDivisibleByFiveTestThree()
+        {
+            int[] inputArray = { 0, 1, 1, 1, 1, 1 };
+            List<bool> expectedResult = new List<bool> { true, false, false, false, true, false };
+
+            Assert.AreEqual(expectedResult, new BinaryPrefixDivisibleByFiveProcessor().PrefixesDivBy5(inputArray));
+        }
+
+        [TestCase()]
+        public void BinaryPrefixDivisibleByFiveTestFour()
+        {
+            int[] inputArray = { 1, 1, 1, 0, 1 };
+            List<bool> expectedResult = new List<bool> { false, false, false, false, false };
+
+            Assert.AreEqual(expectedResult, new BinaryPrefixDivisibleByFiveProcessor().PrefixesDivBy5(inputArray));
+        }
+
+        [TestCase()]
+        public void BinaryPrefixDivisibleByFiveTestFive()
+        {
+            int[] inputArray = { 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1 };
+            List<bool> expectedResult = new List<bool> { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, true, true, true, true, false };
+
+            Assert.AreEqual(expectedResult, new BinaryPrefixDivisibleByFiveProcessor().PrefixesDivBy5(inputArray));
+        }
+    }
+}
