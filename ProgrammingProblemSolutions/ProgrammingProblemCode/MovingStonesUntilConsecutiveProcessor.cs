fc99178ea5e9d6dddee1e766d61b7801262592b4
n-fowler <nfowler587@gmail.com>
2019-04-29 13:10:31 -0400

    Moving stones

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/MovingStonesUntilConsecutiveProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class MovingStonesUntilConsecutiveProcessor
+    {
+        public int[] NumMovesStones(int a, int b, int c)
+        {
+            int[] inputArray = { a, b, c };
+            Array.Sort(inputArray);
+            a = inputArray[0];
+            b = inputArray[1];
+            c = inputArray[2];
+            int maxstep = Math.Abs(c - a) - 2;
+            int minstep;
+
+            if (Math.Abs(b - a) == 1 && Math.Abs(c - b) == 1)
+            {
+                minstep = 0;
+            }
+            else if (Math.Abs(b - a) < 3 || Math.Abs(c - b) < 3)
+            {
+                minstep = 1;
+            }
+            else
+            {
+                minstep = 2;
+            }
+
+            return new[] { minstep, maxstep };
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/MovingStonesUntilConsecutive.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class MovingStonesUntilConsecutive
+    {
+        [TestCase()]
+        public void MovingStonesUntilConsecutiveTestOne()
+        {
+            int a = 1, b = 2, c = 5;
+            int[] expectedOutput = { 1, 2 };
+
+            Assert.AreEqual(expectedOutput, new MovingStonesUntilConsecutiveProcessor().NumMovesStones(a, b, c));
+        }
+
+        [TestCase()]
+        public void MovingStonesUntilConsecutiveTestTwo()
+        {
+            int a = 4, b = 3, c = 2;
+            int[] expectedOutput = { 0, 0 };
+
+            Assert.AreEqual(expectedOutput, new MovingStonesUntilConsecutiveProcessor().NumMovesStones(a, b, c));
+        }
+
+        [TestCase()]
+        public void MovingStonesUntilConsecutiveTestThree()
+        {
+            int a = 2, b = 4, c = 1;
+            int[] expectedOutput = { 1, 1 };
+
+            Assert.AreEqual(expectedOutput, new MovingStonesUntilConsecutiveProcessor().NumMovesStones(a, b, c));
+        }
+    }
+}
