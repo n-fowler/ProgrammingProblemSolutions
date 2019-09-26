2c769b8fe3de914af0b49f678b9d73647e928c3f
n-fowler <nfowler587@gmail.com>
2019-09-26 01:00:14 -0400

    ugly number III

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/UglyNumberProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    class UglyNumberProcessor
+    {
+        public int NthUglyNumber(int n, int a, int b, int c)
+        {
+            int low = 1, high = Int32.MaxValue, mid;
+
+            while (low < high)
+            {
+                mid = low + (high - low) / 2;
+
+                if (Count((a), b, c, mid) < n)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+
+            return high;
+        }
+
+        private long GreatestCommonDivisor(long a, long b)
+        {
+            return a == 0 ? b : GreatestCommonDivisor(b % a, a);
+        }
+
+        private long LowestCommonDenominator(long a, long b)
+        {
+            return (a * b) / GreatestCommonDivisor(a, b);
+        }
+
+        private int Count(long a, long b, long c, long number)
+        {
+            return (int)((number / a) + (number / b) + (number / c)
+                          - (number / LowestCommonDenominator(a, b))
+                          - (number / LowestCommonDenominator(b, c))
+                          - (number / LowestCommonDenominator(a, c))
+                          + (number / LowestCommonDenominator(a, LowestCommonDenominator(b, c))));
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/UglyNumber.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    class UglyNumber
+    {
+        [TestCase(3, 2, 3, 5, ExpectedResult = 4)]
+        [TestCase(4, 2, 3, 4, ExpectedResult = 6)]
+        [TestCase(5, 2, 11, 13, ExpectedResult = 10)]
+        [TestCase(1000000000, 2, 217983653, 336916467, ExpectedResult = 1999999984)]
+        public int UglyNumberTest(int n, int a, int b, int c)
+        {
+            return new UglyNumberProcessor().NthUglyNumber(n, a, b, c);
+        }
+    }
+}
