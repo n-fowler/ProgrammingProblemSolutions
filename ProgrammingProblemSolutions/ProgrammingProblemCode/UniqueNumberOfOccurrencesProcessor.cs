895ea58f36ba72b22313ec300150e8bb9e08f941
n-fowler <nfowler587@gmail.com>
2019-10-02 03:37:28 -0400

    unique number of occurrences problem

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/UniqueNumberOfOccurrencesProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    //Runtime 88ms, faster than 91%, memory usage less than 100%
+    public class UniqueNumberOfOccurrencesProcessor
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            Dictionary<int, int> resultDictionary = new Dictionary<int, int>();
+            foreach (int i in arr)
+            {
+                if (!resultDictionary.ContainsKey(i))
+                {
+                    resultDictionary.Add(i, 1);
+                }
+                else
+                {
+                    resultDictionary[i] += 1;
+                }
+            }
+
+            return resultDictionary.Values.Distinct().Count() >= resultDictionary.Values.Count;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/UniqueNumberOfOccurrences.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class UniqueNumberOfOccurrences
+    {
+        [TestCase()]
+        public void UniqueOccurrencesTestOne()
+        {
+            int[] testArray = { 1, 2, 2, 1, 1, 3 };
+            Assert.IsTrue(new UniqueNumberOfOccurrencesProcessor().UniqueOccurrences(testArray));
+        }
+
+        [TestCase()]
+        public void UniqueOccurrencesTestTwo()
+        {
+            int[] testArray = { 1, 2 };
+            Assert.IsFalse(new UniqueNumberOfOccurrencesProcessor().UniqueOccurrences(testArray));
+        }
+
+        [TestCase()]
+        public void UniqueOccurrencesTestThree()
+        {
+            int[] testArray = { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 };
+            Assert.IsTrue(new UniqueNumberOfOccurrencesProcessor().UniqueOccurrences(testArray));
+        }
+    }
+}
