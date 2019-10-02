a2c06e56f7d9fb29866253a8e84a2b81efbe5209
n-fowler <nfowler587@gmail.com>
2019-10-02 04:29:48 -0400

    remove all adjacent duplicates in string

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/RemoveAllAdjacentDuplicatesInStringTwoProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class RemoveAllAdjacentDuplicatesInStringTwoProcessor
+    {
+        public string RemoveDuplicates(string s, int k)
+        {
+            if (s.Length <= k)
+            {
+                return s;
+            }
+
+            for (int i = 0; i <= s.ToCharArray().Length - k; i++)
+            {
+                if (s.Substring(i, k).Distinct().Count() == 1)
+                {
+                    s = s.Remove(i, k);
+                    return RemoveDuplicates(s, k);
+                }
+            }
+
+            return s;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/RemoveAllAdjacentDuplicatesInStringTwo.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class RemoveAllAdjacentDuplicatesInStringTwo
+    {
+        [TestCase("abcd", 2, ExpectedResult = "abcd")]
+        [TestCase("deeedbbcccbdaa", 3, ExpectedResult = "aa")]
+        [TestCase("pbbcggttciiippooaais", 2, ExpectedResult = "ps")]
+        [TestCase("s", 2, ExpectedResult = "s")]
+        public string RemoveDuplicatesTest(string s, int k)
+        {
+            return new RemoveAllAdjacentDuplicatesInStringTwoProcessor().RemoveDuplicates(s, k);
+        }
+    }
+}
