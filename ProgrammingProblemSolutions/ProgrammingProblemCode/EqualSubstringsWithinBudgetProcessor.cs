eb6e0c086ed745b58ddb56df8837ae22184f876e
n-fowler <nfowler587@gmail.com>
2019-10-03 01:59:49 -0400

    equal substrings within budget

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/EqualSubstringsWithinBudgetProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class EqualSubstringsWithinBudgetProcessor
+    {
+        public int EqualSubstring(string s, string t, int maxCost)
+        {
+            int length = s.Length;
+
+            int left = 0;
+            int index = 0;
+
+            int sum = 0;
+            int max = 0;
+
+            while (index < length)
+            {
+                int diff = Math.Abs(s[index] - t[index]);
+
+                int current = sum + diff;
+                if (current <= maxCost)
+                {
+                    int newlength = index - left + 1;
+                    if (newlength > max)
+                    {
+                        max = newlength;
+                    }
+
+                    sum = current;
+                    index++;
+                }
+
+                while (current > maxCost && left < length)
+                {
+                    int leftValue = Math.Abs(s[left] - t[left]);
+                    sum -= leftValue;
+                    left++;
+                    current -= leftValue;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/EqualSubstringsWithinBudget.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class EqualSubstringsWithinBudget
+    {
+        [TestCase("abcd", "bcdf", 3, ExpectedResult = 3)]
+        [TestCase("abcd", "cdef", 3, ExpectedResult = 1)]
+        [TestCase("abcd", "acde", 0, ExpectedResult = 1)]
+        [TestCase("abcd", "cdef", 1, ExpectedResult = 0)]
+        [TestCase("pxezla", "loewbi", 25, ExpectedResult = 4)]
+        [TestCase("jzmhzdq", "rymuemg", 17, ExpectedResult = 3)]
+        public int EqualSubstringsWithinBudgetTest(string s, string t, int maxCost)
+        {
+            return new EqualSubstringsWithinBudgetProcessor().EqualSubstring(s, t, maxCost);
+        }
+    }
+}
