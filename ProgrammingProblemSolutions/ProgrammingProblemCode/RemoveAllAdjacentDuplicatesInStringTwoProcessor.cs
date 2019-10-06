2d3ae5996389ea5644ee54b3cdb23d40957bee38
n-fowler <nfowler587@gmail.com>
2019-10-06 07:13:01 -0400

    remove all adjacent duplicates in string two

--- ProgrammingProblemSolutions/ProgrammingProblemCode/RemoveAllAdjacentDuplicatesInStringTwoProcessor.cs
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/RemoveAllAdjacentDuplicatesInStringTwoProcessor.cs
@@ -1,22 +1,24 @@
-﻿using System.Linq;
-
-namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
 {
     public class RemoveAllAdjacentDuplicatesInStringTwoProcessor
     {
         public string RemoveDuplicates(string s, int k)
         {
-            if (s.Length <= k)
+            int count = 1;
+            for (int i = 0; i < s.Length - 1; i++)
             {
-                return s;
-            }
 
-            for (int i = 0; i <= s.ToCharArray().Length - k; i++)
-            {
-                if (s.Substring(i, k).Distinct().Count() == 1)
+                if (s[i] == s[i + 1])
                 {
-                    s = s.Remove(i, k);
-                    return RemoveDuplicates(s, k);
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+                if (count == k)
+                {
+                    s = RemoveDuplicates(s.Substring(0, i - k + 2) + s.Substring(i + 2), k);
                 }
             }
 
