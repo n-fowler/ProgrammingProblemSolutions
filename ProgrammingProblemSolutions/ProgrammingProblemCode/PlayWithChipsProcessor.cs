df881ec03852d3d37be1db77aba1604a75fd02ed
n-fowler <nfowler587@gmail.com>
2019-10-06 15:57:31 -0400

    play with chips

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/PlayWithChipsProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class PlayWithChipsProcessor
+    {
+        public int MinCostToMoveChips(int[] chips)
+        {
+            int odd = 0, even = 0;
+
+            foreach (int chip in chips)
+            {
+                if (chip % 2 == 1)
+                {
+                    odd++;
+                }
+                else
+                {
+                    even++;
+                }
+            }
+
+            return Math.Min(odd, even);
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/PlayWithChips.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class PlayWithChips
+    {
+        [TestCase()]
+        public void PlayWithChipsTestOne()
+        {
+            int[] chips = { 1, 2, 3 };
+            Assert.AreEqual(1, new PlayWithChipsProcessor().MinCostToMoveChips(chips));
+        }
+
+        [TestCase()]
+        public void PlayWithChipsTestTwo()
+        {
+            int[] chips = { 2, 2, 2, 3, 3 };
+            Assert.AreEqual(2, new PlayWithChipsProcessor().MinCostToMoveChips(chips));
+        }
+    }
+}
