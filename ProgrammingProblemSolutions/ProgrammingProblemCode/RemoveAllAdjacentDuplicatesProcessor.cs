2ae377a330d2c9a7f18ee3207e9096439737bdf8
n-fowler <nfowler587@gmail.com>
2019-10-06 07:37:30 -0400

    Remove all adjacent duplicates one

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/RemoveAllAdjacentDuplicatesProcessor.cs
@@ -0,0 +1,18 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class RemoveAllAdjacentDuplicatesProcessor
+    {
+        public string RemoveDuplicates(string S)
+        {
+            for (int i = 0; i < S.Length - 1; i++)
+            {
+                if (S[i] == S[i + 1])
+                {
+                    S = RemoveDuplicates(S.Substring(0, i) + S.Substring(i + 2));
+                }
+            }
+
+            return S;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/RemoveAllAdjacentDuplicatesOne.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class RemoveAllAdjacentDuplicatesOne
+    {
+        [TestCase("abbaca", ExpectedResult = "ca")]
+        public string RemoveAllAdjacentDuplicatesTest(string input)
+        {
+            return new RemoveAllAdjacentDuplicatesProcessor().RemoveDuplicates(input);
+        }
+    }
+}
