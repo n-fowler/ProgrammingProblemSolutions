4291c381866a430175c4c4676cc130aa9dc31f6b
n-fowler <nfowler587@gmail.com>
2019-10-08 15:08:36 -0400

    get day of week (without using built in functionality)

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/DayOfTheWeekProcessor.cs
@@ -0,0 +1,36 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class DayOfTheWeekProcessor
+    {
+        public string DayOfTheWeek(int day, int month, int year)
+        {
+            int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+            year -= (month < 3) ? 1 : 0;
+
+            return GetDayOfWeek((year + year / 4 - year / 100 + year / 400 + t[month - 1] + day) % 7);
+        }
+
+        private string GetDayOfWeek(int dayOfWeekInt)
+        {
+            switch (dayOfWeekInt)
+            {
+                case 0:
+                    return "Sunday";
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/DayOfTheWeek.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class DayOfTheWeek
+    {
+        [TestCase(31, 8, 2019, ExpectedResult = "Saturday")]
+        [TestCase(18, 7, 1999, ExpectedResult = "Sunday")]
+        [TestCase(15, 8, 1993, ExpectedResult = "Sunday")]
+        public string DayOfTheWeekTest(int day, int month, int year)
+        {
+            return new DayOfTheWeekProcessor().DayOfTheWeek(day, month, year);
+        }
+    }
+}
