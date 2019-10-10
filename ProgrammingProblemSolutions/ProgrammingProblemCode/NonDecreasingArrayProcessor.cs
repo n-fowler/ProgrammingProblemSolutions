f50f86d8930c2fe27321c6cd93c474ef62e630e7
n-fowler <nfowler587@gmail.com>
2019-10-10 16:58:03 -0400

    non decreasing array

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/NonDecreasingArrayProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class NonDecreasingArrayProcessor
+    {
+        public bool CheckPossibility(int[] nums)
+        {
+            bool spike = false;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] - nums[i - 1] < 0)
+                {
+                    if (spike)
+                    {
+                        return false;
+                    }
+                    spike = true;
+
+                    if (i + 1 < nums.Length)
+                    {
+                        if (nums[i + 1] >= nums[i - 1])
+                        {
+                            nums[i] = nums[i - 1];
+                        }
+                        else if (nums[i + 1] < nums[i])
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            nums[i - 1] = nums[i];
+                            i = Math.Max(0, i - 2);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/NonDecreasingArray.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class NonDecreasingArray
+    {
+        [TestCase()]
+        public void NonDecreasingArrayTestOne()
+        {
+            int[] input = { 4, 2, 3 };
+            Assert.IsTrue(new NonDecreasingArrayProcessor().CheckPossibility(input));
+        }
+
+        [TestCase()]
+        public void NonDecreasingArrayTestTwo()
+        {
+            int[] input = { 4, 2, 1 };
+            Assert.IsFalse(new NonDecreasingArrayProcessor().CheckPossibility(input));
+        }
+
+        [TestCase()]
+        public void NonDecreasingArrayTestThree()
+        {
+            int[] input = { 3, 4, 2, 3 };
+            Assert.IsFalse(new NonDecreasingArrayProcessor().CheckPossibility(input));
+        }
+
+        [TestCase()]
+        public void NonDecreasingArrayTestFour()
+        {
+            int[] input = { -1, 4, 2, 3 };
+            Assert.IsTrue(new NonDecreasingArrayProcessor().CheckPossibility(input));
+        }
+
+        [TestCase()]
+        public void NonDecreasingArrayTestFive()
+        {
+            int[] input = { 1, 3, 2 };
+            Assert.IsTrue(new NonDecreasingArrayProcessor().CheckPossibility(input));
+        }
+    }
+}
