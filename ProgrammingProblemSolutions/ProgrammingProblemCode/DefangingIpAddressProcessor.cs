d9bc7eecd418f116ef117d5b6a82b3c2155524ce
n-fowler <nfowler587@gmail.com>
2019-10-13 11:55:12 -0400

    defanging ip address

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/DefangingIpAddressProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class DefangingIpAddressProcessor
+    {
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/DefangingIpAddress.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class DefangingIpAddress
+    {
+        [TestCase("1.1.1.1", ExpectedResult = "1[.]1[.]1[.]1")]
+        public string DefangingIpAddressTest(string input)
+        {
+            return new DefangingIpAddressProcessor().DefangIPaddr(input);
+        }
+    }
+}
