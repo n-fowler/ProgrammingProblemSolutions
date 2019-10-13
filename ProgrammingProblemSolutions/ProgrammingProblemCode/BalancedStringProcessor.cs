0538ed2487318c1c3d2fccb856380fed9833c9be
n-fowler <nfowler587@gmail.com>
2019-10-13 12:04:47 -0400

    balanced strings

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/BalancedStringProcessor.cs
@@ -0,0 +1,33 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class BalancedStringProcessor
+    {
+        public int BalancedStringSplit(string s)
+        {
+            int lCount = 0;
+            int rCount = 0;
+            int splitCount = 0;
+
+            foreach (char c in s)
+            {
+                if (c == 'L')
+                {
+                    lCount++;
+                }
+                else
+                {
+                    rCount++;
+                }
+
+                if (lCount == rCount)
+                {
+                    splitCount++;
+                    lCount = 0;
+                    rCount = 0;
+                }
+            }
+
+            return splitCount;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/BalancedString.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture]
+    public class BalancedString
+    {
+        [TestCase("RLRRLLRLRL", ExpectedResult = 4)]
+        [TestCase("RLLLLRRRLR", ExpectedResult = 3)]
+        [TestCase("LLLLRRRR", ExpectedResult = 1)]
+        public int BalancedStringTest(string input)
+        {
+            return new BalancedStringProcessor().BalancedStringSplit(input);
+        }
+    }
+}
