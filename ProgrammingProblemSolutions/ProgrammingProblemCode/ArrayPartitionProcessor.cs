50b55f41d38b6b6749389cabaa5d337660585502
n-fowler <nfowler587@gmail.com>
2019-10-13 12:17:46 -0400

    Array Partition

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ArrayPartitionProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ArrayPartitionProcessor
+    {
+        public int ArrayPairSum(int[] nums)
+        {
+            int sum = 0;
+
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += nums[i];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ArrayPartition.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ArrayPartition
+    {
+        [TestCase()]
+        public void ArrayPartitionTest()
+        {
+            int[] input = { 1, 4, 3, 2 };
+            Assert.AreEqual(4, new ArrayPartitionProcessor().ArrayPairSum(input));
+        }
+    }
+}
