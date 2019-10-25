e88b8d5da5f52b10505a9da7cc57c211ebb4a3a6
n-fowler <nfowler587@gmail.com>
2019-10-24 23:21:16 -0400

    matryoshka doll

---
+++ ProgrammingProblemSolutions/InterviewProblems/MatryoshkaDoll.cs
@@ -0,0 +1,28 @@
+﻿public class MatryoshkaDoll
+{
+    private readonly MatryoshkaDoll containedDoll;
+
+    public MatryoshkaDoll() { }
+
+    public MatryoshkaDoll(MatryoshkaDoll containedDoll)
+    {
+        this.containedDoll = containedDoll;
+    }
+
+    public int NumberOfSmallerDolls
+    {
+        get
+        {
+            int count = 0;
+            MatryoshkaDoll _containedDoll = new MatryoshkaDoll(containedDoll);
+
+            while (_containedDoll != null)
+            {
+                count++;
+                _containedDoll = _containedDoll.containedDoll;
+            }
+
+            return count - 1; //Don't count original
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/MatryoshkaDollTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class MatryoshkaDollTest
+    {
+        [TestCase()]
+        public void MatryoshkaDollTesting()
+        {
+            Assert.AreEqual(1, new MatryoshkaDoll(new MatryoshkaDoll()).NumberOfSmallerDolls);
+            Assert.AreEqual(2, new MatryoshkaDoll(new MatryoshkaDoll(new MatryoshkaDoll())).NumberOfSmallerDolls);
+        }
+    }
+}
