dd2c0bb1faef2dbf838df90775920fce6d7795a4
n-fowler <nfowler587@gmail.com>
2019-10-24 23:57:31 -0400

    SKYLINE

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/MaxIncreaseToKeepCitySkylineProcessor.cs
@@ -0,0 +1,62 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class MaxIncreaseToKeepCitySkylineProcessor
+    {
+        public int MaxIncreaseKeepingSkyline(int[][] grid)
+        {
+            int[] leftToRight = EvaluateLeftRight(grid);
+            int[] topToBottom = EvaluateTopBottom(grid);
+            int howMuchTaller = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int x = 0; x < grid.Length; x++)
+                {
+                    while (grid[i][x] < leftToRight[i] && grid[i][x] < topToBottom[x])
+                    {
+                        howMuchTaller++;
+                        grid[i][x]++;
+                    }
+                }
+            }
+            return howMuchTaller;
+        }
+
+        private int[] EvaluateLeftRight(int[][] grid)
+        {
+            int[] leftRightArray = new int[grid.Length];
+            int tempHeight = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int x = 0; x < grid.Length; x++)
+                {
+                    if (grid[i][x] >= tempHeight)
+                    {
+                        leftRightArray[i] = grid[i][x];
+                        tempHeight = grid[i][x];
+                    }
+                }
+                tempHeight = 0;
+            }
+            return leftRightArray;
+        }
+
+        private int[] EvaluateTopBottom(int[][] grid)
+        {
+            int[] topBottomArray = new int[grid.Length];
+            int tempHeight = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int x = 0; x < grid.Length; x++)
+                {
+                    if (grid[x][i] >= tempHeight)
+                    {
+                        topBottomArray[i] = grid[x][i];
+                        tempHeight = grid[x][i];
+                    }
+                }
+                tempHeight = 0;
+            }
+            return topBottomArray;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/MaxIncreaseToKeepCitySkyline.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class MaxIncreaseToKeepCitySkyline
+    {
+        [TestCase()]
+        public void MaxIncreaseToKeepCitySkylineTest()
+        {
+            int[][] grid =
+            {
+                new[] { 3, 0, 8, 4 },
+                new[] { 2, 4, 5, 7 },
+                new[] { 9, 2, 6, 3 },
+                new[] { 0, 3, 1, 0 }
+            };
+
+            Assert.AreEqual(35, new MaxIncreaseToKeepCitySkylineProcessor().MaxIncreaseKeepingSkyline(grid));
+        }
+    }
+}
