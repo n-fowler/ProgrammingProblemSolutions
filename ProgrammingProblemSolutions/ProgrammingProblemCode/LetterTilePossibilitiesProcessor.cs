fbc7ba558420f88376f15e7baf0c25c19255cc09
n-fowler <nfowler587@gmail.com>
2019-10-26 20:50:28 -0400

    Letter Tile Possibilities

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/LetterTilePossibilitiesProcessor.cs
@@ -0,0 +1,37 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class LetterTilePossibilitiesProcessor
+    {
+        private static int MAX_CHAR = 26;
+
+        public int NumTilePossibilities(string tiles)
+        {
+            int[] count = new int[MAX_CHAR];
+
+            foreach (char tile in tiles)
+            {
+                count[tile - 'A']++;
+            }
+
+            return Backtrack(count);
+        }
+
+        private int Backtrack(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < MAX_CHAR; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    continue;
+                }
+
+                sum++;
+                arr[i]--;
+                sum += Backtrack(arr);
+                arr[i]++;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/LetterTilePossibilities.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class LetterTilePossibilities
+    {
+        [TestCase("AAB", ExpectedResult = 8)]
+        [TestCase("AAABBC", ExpectedResult = 188)]
+        public int LetterTilePossibilitiesTest(string tiles)
+        {
+            return new LetterTilePossibilitiesProcessor().NumTilePossibilities(tiles);
+        }
+    }
+}
