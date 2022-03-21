fd626daf9f89db4cd7b74b8c13b10e2cd52e7bd5
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-03-21 12:08:11 -0400

    running sum

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/RunningSumProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class RunningSumProcessor
+    {
+        public int[] RunningSum(int[] nums)
+        {            
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/RunningSum.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class RunningSum
+    {
+        [TestCase()]
+        public void RunningSumTestOne()
+        {
+            int[] input = { 1, 2, 3, 4 };
+            int[] output = new RunningSumProcessor().RunningSum(input);
+
+            int[] expectedOutput = new int[] { 1, 3, 6, 10 };
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestCase()]
+        public void RunningSumTestTwo()
+        {
+            int[] input = { 1, 1, 1, 1, 1 };
+            int[] output = new RunningSumProcessor().RunningSum(input);
+
+            int[] expectedOutput = new int[] { 1, 2, 3, 4, 5 };
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestCase()]
+        public void RunningSumTestThree()
+        {
+            int[] input = { 3, 1, 2, 10, 1 };
+            int[] output = new RunningSumProcessor().RunningSum(input);
+
+            int[] expectedOutput = new int[] { 3, 4, 6, 16, 17 };
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+    }
+}
