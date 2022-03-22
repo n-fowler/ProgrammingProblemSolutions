ddae3da29899a076f324c0edff7b037f35fc5053
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-03-22 16:10:06 -0400

    Shuffle the array

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ShuffleTheArrayProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ShuffleTheArrayProcessor
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            int[] result = new int[nums.Length];
+
+            for (int i = 0, j = 0; i < nums.Length/2; i++)
+            {
+                result[j++] = nums[i];
+                result[j++] = nums[n];
+                n++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ShuffleTheArray.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ShuffleTheArray
+    {
+        [TestCase()]
+        public void ShuffleTheArrayTestOne()
+        {
+            int[] input = new int[] { 2, 5, 1, 3, 4, 7 };
+            int inputN = 3;
+            int[] expectedOutput = new int[] { 2, 3, 5, 4, 1, 7 };
+
+            int[] actualOutput = new ShuffleTheArrayProcessor().Shuffle(input, inputN);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestCase()]
+        public void ShuffleTheArrayTestTwo()
+        {
+            int[] input = new int[] { 1, 2, 3, 4, 4, 3, 2, 1 };
+            int inputN = 4;
+            int[] expectedOutput = new int[] { 1, 4, 2, 3, 3, 2, 4, 1 };
+
+            int[] actualOutput = new ShuffleTheArrayProcessor().Shuffle(input, inputN);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestCase()]
+        public void ShuffleTheArrayTestThree()
+        {
+            int[] input = new int[] { 1, 1, 2, 2 };
+            int inputN = 2;
+            int[] expectedOutput = new int[] { 1, 2, 1, 2 };
+
+            int[] actualOutput = new ShuffleTheArrayProcessor().Shuffle(input, inputN);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
