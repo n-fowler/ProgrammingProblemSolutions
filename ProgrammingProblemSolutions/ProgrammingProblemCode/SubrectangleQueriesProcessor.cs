ff207c8dcb1d83da49b6485f0bde50780135d0a7
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-03-23 15:24:17 -0400

    Subrectangle Queries

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/SubrectangleQueriesProcessor.cs
@@ -0,0 +1,28 @@
+﻿namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class SubrectangleQueriesProcessor
+    {
+        int[][] rectangle;
+
+        public SubrectangleQueriesProcessor(int[][] rectangle)
+        {
+            this.rectangle = rectangle;
+        }
+
+        public void UpdateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+        {
+            for (int r = row1; r <= row2; r++)
+            {
+                for (int c = col1; c <= col2; c++)
+                {
+                    rectangle[r][c] = newValue;
+                }
+            }
+        }
+
+        public int GetValue(int row, int col)
+        {
+            return rectangle[row][col];
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/SubrectangleQueries.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class SubrectangleQueries
+    {
+        [TestCase()]
+        public void SubrectangleQueriesTest()
+        {
+            int[][] subrectangle = new int[][] 
+            {
+                new[] { 1, 2, 1 }, 
+                new[] { 4, 3, 4 }, 
+                new[] { 3, 2, 1 }, 
+                new[] { 1, 1, 1 } 
+            };
+            var subrectangleQueries = new SubrectangleQueriesProcessor(subrectangle);
+            Assert.AreEqual(1, subrectangleQueries.GetValue(0, 2));
+            subrectangleQueries.UpdateSubrectangle(0, 0, 3, 2, 5);
+            Assert.AreEqual(5, subrectangleQueries.GetValue(0, 2));
+            Assert.AreEqual(5, subrectangleQueries.GetValue(3, 1));
+            subrectangleQueries.UpdateSubrectangle(3, 0, 3, 2, 10);
+            Assert.AreEqual(10, subrectangleQueries.GetValue(3, 1));
+            Assert.AreEqual(5, subrectangleQueries.GetValue(0, 2));
+        }
+    }
+}
