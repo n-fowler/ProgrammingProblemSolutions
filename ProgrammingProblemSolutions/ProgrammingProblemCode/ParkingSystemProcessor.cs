f1c516f8a746595c1d89919629d531eb4bbc3d35
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-03-24 11:20:39 -0400

    parking system

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/ParkingSystemProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class ParkingSystemProcessor
+    {
+        private int[] sizes = {0,0,0,0};
+
+        public ParkingSystemProcessor(int big, int medium, int small)
+        {
+            sizes[0] = 0;
+            sizes[1] = big;
+            sizes[2] = medium;
+            sizes[3] = small;
+        }
+
+        public bool AddCar(int carType)
+        {
+            if(sizes[carType] > 0)
+            {
+                sizes[carType]--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/ParkingSystem.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class ParkingSystem
+    {
+        [TestCase()]
+        public void ParkingSystemTest()
+        {
+            var parkingSystem = new ParkingSystemProcessor(1, 1, 0);
+            Assert.IsTrue(parkingSystem.AddCar(1)); // return true because there is 1 available slot for a big car
+            Assert.IsTrue(parkingSystem.AddCar(2)); // return true because there is 1 available slot for a medium car
+            Assert.IsFalse(parkingSystem.AddCar(3)); // return false because there is no available slot for a small car
+            Assert.IsFalse(parkingSystem.AddCar(1)); // return false because there is no available slot for a big car. It is already occupied.
+        }
+    }
+}
