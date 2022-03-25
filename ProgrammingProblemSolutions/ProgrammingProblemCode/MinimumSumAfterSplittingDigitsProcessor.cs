afe4620ec75030e30d0e54ebb06eca87adbb42c1
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-03-25 13:45:18 -0400

    Minimum sum after splitting digits

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/MinimumSumAfterSplittingDigitsProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class MinimumSumAfterSplittingDigitsProcessor
+    {
+        public int MinimumSum(int num)
+        {
+            string numStr = num.ToString();
+            int numStrLength = numStr.Length;
+            int[] digitArray = new int[numStrLength];
+
+            for (int i = 0; i < numStrLength; i++)
+            {
+                digitArray[i] = numStr[i] - 48;
+            }
+
+            Array.Sort(digitArray);
+
+            return digitArray[0]*10 + digitArray[1]*10 + digitArray[2] + digitArray[3];
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/MinimumSumAfterSplittingDigits.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class MinimumSumAfterSplittingDigits
+    {
+        [TestCase(2932, ExpectedResult = 52)]
+        [TestCase(4009, ExpectedResult = 13)]
+        public int MinimumSumAfterSplittingDigitsTestOne(int input)
+        {
+            return new MinimumSumAfterSplittingDigitsProcessor().MinimumSum(input);
+        }
+    }
+}
