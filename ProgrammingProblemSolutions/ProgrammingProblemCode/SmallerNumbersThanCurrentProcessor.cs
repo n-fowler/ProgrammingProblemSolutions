fddb38b0a07e6b2aeca581e2cc761dfd51333dfe
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-03-28 14:27:22 -0400

    smaller numbers than current

---
+++ ProgrammingProblemSolutions/ProgrammingProblemCode/SmallerNumbersThanCurrentProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.ProgrammingProblemCode
+{
+    public class SmallerNumbersThanCurrentProcessor
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int smallerCount = 0;
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if(nums[i] > nums[j])
+                    {
+                        smallerCount++;
+                    }
+                }
+
+                result[i] = smallerCount;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProgrammingProblemSolutions/UnitTests/SmallerNumbersThanCurrent.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using ProgrammingProblemSolutions.ProgrammingProblemCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingProblemSolutions.UnitTests
+{
+    [TestFixture()]
+    public class SmallerNumbersThanCurrent
+    {
+        [TestCase()]
+        public void SmallerNumbersThanCurrentTestOne()
+        {
+            int[] input = { 8, 1, 2, 2, 3 };
+            int[] expectedOutput = { 4, 0, 1, 1, 3 };
+
+            int[] actualOutput = new SmallerNumbersThanCurrentProcessor().SmallerNumbersThanCurrent(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestCase()]
+        public void SmallerNumbersThanCurrentTestTwo()
+        {
+            int[] input = { 6, 5, 4, 8 };
+            int[] expectedOutput = { 2, 1, 0, 3 };
+
+            int[] actualOutput = new SmallerNumbersThanCurrentProcessor().SmallerNumbersThanCurrent(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestCase()]
+        public void SmallerNumbersThanCurrentTestThree()
+        {
+            int[] input = { 7, 7, 7, 7 };
+            int[] expectedOutput = { 0, 0, 0, 0 };
+
+            int[] actualOutput = new SmallerNumbersThanCurrentProcessor().SmallerNumbersThanCurrent(input);
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
